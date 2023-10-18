1bf7c530d4c0838afff7dc078058774d0ac12b51
shylabo <shuya.sohot@gmail.com>
2023-10-17 21:53:35 -0700

    feat: authentication

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 'use client'
+import { useConvexAuth } from 'convex/react'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
 
 import { useScrollTop } from '@/hooks/use-scroll-top'
 import { cn } from '@/lib/utils'
 import { Logo } from './logo'
 import { ModeToggle } from '@/components/mode-toggle'
+import { Button } from '@/components/ui/button'
+import { Spinner } from '@/components/spinner'
+import Link from 'next/link'
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -16,8 +22,30 @@ export const Navbar = () => {
       )}
     >
       <Logo />
-      <div className="w-full flex justify-between items-center md:justify-end gap-x-2 md:ml-auto">Login</div>
-      <ModeToggle />
+      <div className="w-full flex justify-between items-center md:justify-end gap-x-2 md:ml-auto">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+        <ModeToggle />
+      </div>
     </div>
   )
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://intent-bream-68.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+}
