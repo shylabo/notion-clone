8e793b92741ed812e2ade91f26d903d9d31fa6c2
shylabo <shuya.sohot@gmail.com>
2023-10-18 22:25:33 -0700

    feat: banner component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,8 @@ import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { MenuIcon } from 'lucide-react'
 import { Title } from './title'
+import { Banner } from './banner'
+import { Menu } from './menu'
 
 interface NavbarProps {
   isCollapsed: boolean
@@ -22,8 +24,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -38,8 +43,12 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         {isCollapsed && <MenuIcon role="button" onClick={onResetWidth} className="h-6 w-6 text-muted-foreground" />}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   )
 }
--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -48,8 +48,8 @@ export const Title = ({ initialData }: TitleProps) => {
   }
 
   return (
-    <>
-      <div className="flex items-center gap-x-1">{!!initialData.icon && <p>{initialData.icon}</p>}</div>
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
         <Input
           ref={inputRef}
@@ -65,7 +65,7 @@ export const Title = ({ initialData }: TitleProps) => {
           <span className="truncate">{initialData.title}</span>
         </Button>
       )}
-    </>
+    </div>
   )
 }
 
